7efda4c8b910e76ac6cf847b6291b8559fadaae7
saulcalla <saulcalla@gmail.com>
2017-12-30 08:28:28 -0300

    inicio

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping("/hello")
+    public Map<String, String> hello() {
+        HashMap mapa =  new HashMap();
+        
+        mapa.put("clave", "valor");
+        
+        return mapa;
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/StsSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsSampleApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StsSampleApplication.class, args);
+	}
+}
